5773059f734b54c42a9b2b48fd32cfee008565b7
Marcin Polak <mapoart@gmail.com>
2020-01-15 19:51:23 +0100

    Added handling for NexssP projects

--- templates/default.erl.js
+++ templates/default.erl.js
@@ -2,8 +2,11 @@
 // in this case library needs JSON
 const config = {
   files: ["mochijson2.erl"],
-  // We copy to previous folder as didn't work from current folder.
-  commands: ["move src\\mochijson2.erl .", "erlc mochijson2.erl"],
+  // We copy to previous folder as didn't work from current folder (Only on Nexss Programmer Projects).
+  commands: [
+    "IF exist src (move src\\mochijson2.erl .)",
+    "erlc mochijson2.erl"
+  ],
   repos: ["https://github.com/mochi/mochiweb"],
   descriptions: [
     `It uses mochijson2.erl which is part of Mochiweb
