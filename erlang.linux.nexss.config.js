c8863b4c7a4c7124feac90ad5cf24264e374f5e4
Marcin Polak <mapoart@gmail.com>
2021-04-26 15:28:12 +0200

    Untuntu 18 -> Installer from erlang solutions.

--- erlang.linux.nexss.config.js
+++ erlang.linux.nexss.config.js
@@ -1,4 +1,5 @@
 let languageConfig = Object.assign({}, require("./erlang.win32.nexss.config"));
+const sudo = process.sudo;
 languageConfig.compilers = {
   erlang: {
     install: `apt install -y erlang`,
@@ -8,11 +9,7 @@ languageConfig.compilers = {
   },
 };
 
-const {
-  replaceCommandByDist,
-  dist,
-} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
-const distName = dist();
+const distName = process.distro;
 languageConfig.dist = distName;
 
 // TODO: Later to cleanup this config file !!
@@ -20,8 +17,14 @@ switch (distName) {
   // case "Alpine Linux":
   //   languageConfig.compilers.elixir.install = `apk add erlang`;
   //   break;
+  case process.distros.UBUNTU:
+    languageConfig.compilers.erlang.install = `${sudo}apt-get -y install wget
+${sudo}wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && ${sudo} dpkg -i erlang-solutions_2.0_all.deb
+${sudo}apt-get -y update
+${sudo}apt-get -y install esl-erlang`;
+    break;
   default:
-    languageConfig.compilers.erlang.install = replaceCommandByDist(
+    languageConfig.compilers.erlang.install = process.replacePMByDistro(
       "apt update && apt install -y erlang"
     );
     break;
